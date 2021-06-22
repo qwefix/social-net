59930e460ac542df0fa572c5e2614dd4059b3992
Hramchenlo Alexey <qwefix@gmail.com>
2021-06-22 21:33:28 +0300

    feat:structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import logoIcon from './header-icon.svg';
+// import wallpaper from './wallpaper.jpg';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <header>
+        <img src={logoIcon} width='40' alt='logo' />
+        <p>Header</p>
+        <p>Inform</p>
+      </header>
+      <aside>
+        <nav>
+          <ul>
+            <li>Profile</li>
+            <li>Messages</li>
+            <li>News</li>
+            <li>Music</li>
+            <li>Settings</li>
+          </ul>
+        </nav>
+      </aside>
+      <main>
+        <div className="wallpaper" />
+        <div className='ava' >
+          ava+desc
+        </div>
+        <div className='my-posts'>
+          my posts
+          <div>
+            Post
+          </div>
+          <div>
+            Post
+          </div>
+          <div>
+            Post
+          </div>
+        </div>
+      </main>
     </div>
   );
 }

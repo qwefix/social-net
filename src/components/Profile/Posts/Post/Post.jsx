d203d08a5cabed348ffcedbec520407c309c9044
Alexey <qwefix@gmail.com>
2021-06-29 18:45:19 +0300

    some style

--- src/components/Profile/Posts/Post/Post.jsx
+++ src/components/Profile/Posts/Post/Post.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import c from './Post.module.css';
+import ava from '../../ava.jpg';
 
 const Post = () => {
     return (
         <div className={c.post}>
-            Post
+            <div className={c.ava}>
+                <img src={ava} alt="avatar" />
+            </div>
+            <div className={c.content}>
+                <p className={c.user_name}>Darya Bazhenova</p>
+            </div>
         </div >
     )
 }
--- src/components/Profile/ProfileHeader/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader/ProfileHeader.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import c from './ProfileHeader.module.css';
-import wallpaperImg from'./wallpaper.jpg';
+import wallpaperImg from './wallpaper.jpg';
+import ava from '../ava.jpg'
 
 const ProfileHeader = () => {
     return (
         <header className={c.header}>
-            <div className = {c.wallpaper_box}>
+            <div className={c.wallpaper_box}>
                 <img src={wallpaperImg} alt='wallpaper' className={c.wallpaper} />
             </div>
-            <div className={c.ava} >
-                ava+desc
+            <div className={c.ava_description} >
+                <div className={c.ava}>
+                    <img src={ava} alt="avatar" />
+                </div>
+                <div className={c.description}>
+                    <p>
+                        Darya Bazhenova
+                    </p>
+                </div>
             </div>
         </header>
-
     )
 };
 

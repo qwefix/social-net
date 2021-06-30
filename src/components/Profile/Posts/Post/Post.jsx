33124878b23c47de4308acf38622639552a63eba
Hramchenlo Alexey <qwefix@gmail.com>
2021-06-30 09:12:58 +0300

    like and some style

--- src/components/Profile/Posts/Post/Post.jsx
+++ src/components/Profile/Posts/Post/Post.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import c from './Post.module.css';
 import ava from '../../ava.jpg';
+import likeImg from './like.png'
 
 const Post = () => {
+    function like({target}){
+        target.closest(`.${c.like_box}`).classList.toggle(`${c.active}`);
+    };
     return (
         <div className={c.post}>
             <div className={c.ava}>
@@ -10,6 +14,10 @@ const Post = () => {
             </div>
             <div className={c.content}>
                 <p className={c.user_name}>Darya Bazhenova</p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, laudantium alias quod dolore tempore atque eveniet porro aspernatur neque illum aliquid quam magni dolorem eum molestiae vel. Illo nesciunt optio esse asperiores!
+            </div>
+            <div className={c.like_box} onClick={like}>
+                <img src={likeImg} alt="like" />
             </div>
         </div >
     )

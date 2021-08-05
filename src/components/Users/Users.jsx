87fe40a9cb0d70d5a4789ac8caa17c89b63d8419
Hramchenlo Alexey <qwefix@gmail.com>
2021-08-05 21:32:22 +0300

    rest api lesson 51

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,55 +1,15 @@
+import * as axios from 'axios'
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import c from './Users.module.css'
+import userPhotoHolder from '../../assets/ph/ava.jpg'
 
 export default function Users({ users, addUsers, follow, unfollow }) {
     if (users.length === 0) {
-        addUsers([
-            {
-                id: 1,
-                name: "General grievous",
-                ava: require(`../../redux/1/ava.jpg`).default,
-                status: "boss of this gym",
-                location: {
-                    city: 'Minsk',
-                    country: 'Belarus'
-                },
-                followed: false,
-            },
-            {
-                id: 2,
-                name: "General Kenobi",
-                ava: require(`../../redux/2/ava.jpg`).default,
-                status: 'Best teacher',
-                location: {
-                    city: 'Moscow',
-                    country: 'Russia'
-                },
-                followed: true,
-            },
-            {
-                id: 10,
-                name: "User Placeholder",
-                ava: require(`../../redux/ph/ava.jpg`).default,
-                status: "one of empty users",
-                location: {
-                    city: 'city',
-                    country: 'country'
-                },
-                followed: false,
-            },
-            {
-                id: 11,
-                name: "User Placeholder",
-                ava: require(`../../redux/ph/ava.jpg`).default,
-                status: "one of empty users",
-                location: {
-                    city: 'city',
-                    country: 'country'
-                },
-                followed: false,
-            },
-        ])
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        .then(response=>{
+            addUsers(response.data.items)
+        })
     }
     return (
         <div className={c.user_list}>
@@ -57,7 +17,7 @@ export default function Users({ users, addUsers, follow, unfollow }) {
                 <div className={c.name}>{a.name}</div>
                 <NavLink className={c.ava} to={`/profile/${a.id}`}>
                     <div>
-                        <img src={a.ava} alt="" />
+                        <img src={a.photos.large || userPhotoHolder} alt="" />
                     </div>
                 </NavLink>
                 {a.followed ?
@@ -81,8 +41,8 @@ export default function Users({ users, addUsers, follow, unfollow }) {
                     {a.followed ? 'unfollow' : 'Follow'}
                 </div>
                 <div className={c.location}>
-                    <div>{a.location.city}</div>
-                    <div>{a.location.country}</div>
+                    <div>{'a.location.city'}</div>
+                    <div>{'a.location.country'}</div>
                 </div>
                 <div className={c.status}>
                     {a.status}

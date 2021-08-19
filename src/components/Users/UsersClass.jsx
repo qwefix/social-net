500c8752ec33bff9dad51a1f96e57feb6ce4ca03
Hramchenlo Alexey <qwefix@gmail.com>
2021-08-19 20:12:25 +0300

    l 62 update

--- src/components/Users/UsersClass.jsx
+++ src/components/Users/UsersClass.jsx
@@ -6,21 +6,23 @@ import responseSettings from '../../responceSettings.json'
 export default class UsersClass extends React.Component {
     follow = (id) => {
         this.props.setSpinner(true)
-        axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {},responseSettings )
+        axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {}, responseSettings)
             .then(response => {
-                if (response.data.resultCode===0) {
+                if (response.data.resultCode === 0) {
                     this.props.setSpinner(false)
                     this.props.follow(id)
-            }})
+                }
+            })
     }
     unfollow = (id) => {
         this.props.setSpinner(true)
-        axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${id}`,responseSettings )
+        axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, responseSettings)
             .then(response => {
-                if (response.data.resultCode===0) {
+                if (response.data.resultCode === 0) {
                     this.props.setSpinner(false)
                     this.props.unfollow(id)
-            }})
+                }
+            })
     }
     componentDidMount() {
         if (!this.props.users.length) this.setPage(1);
@@ -29,7 +31,10 @@ export default class UsersClass extends React.Component {
         this.props.selectPage(page)
         this.props.setSpinner(true)
         this.lastPromice = page;
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=10`,{"withCredentials": true ,})
+        axios.get(
+            `https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=10`,
+            { "withCredentials": true, }
+        )
             .then(response => {
                 if (page === this.lastPromice) {
                     this.props.setUsers(response)
